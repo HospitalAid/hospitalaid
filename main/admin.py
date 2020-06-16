f78e3ac3f4b74a0add28affbee2a62f6b726c4d6
Federico Marani <flagzeta@gmail.com>
2020-06-16 22:37:31 +0200

    Cleaned up Django admin

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 
 from main import models
 
-admin.site.register(models.main.ImportedHospital)
-admin.site.register(models.main.Hospital)
-admin.site.register(models.main.AidRequest)
-admin.site.register(models.user.User)
+
+class HospitalInline(admin.StackedInline):
+    model = models.main.Hospital
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = (HospitalInline,)
+
+
+class AidAdmin(admin.ModelAdmin):
+    list_display = ("title", "type", "status")
+    list_filter = ("hospital",)
+
+
+admin.site.register(models.main.AidRequest, AidAdmin)
+admin.site.register(models.user.User, UserAdmin)
